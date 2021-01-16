0af81fb7decea8923fd457cbf210af0f6dd8f269
Bharath Vittal <v.bharath@icloud.com>
2021-01-16 14:43:02 +0530

    Add files via upload

---
+++ doubleminusone/R/doubleminusone.R
@@ -0,0 +1,39 @@
+#' @title doubleminusone
+#'
+#' @description Doubles a number and subtracts 1
+#' @param value
+#'
+#' @return value
+#'
+#' @examples
+#' package:: doubleminusone(n)
+#'
+#' @export
+
+doubleminusone<-function(n)
+{n=n*2-1
+return(n)}
+
+
+
+
+
+
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
